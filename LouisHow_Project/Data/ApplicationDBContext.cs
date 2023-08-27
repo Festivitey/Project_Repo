bb4fd5cab73fcb618ad8073b26439fde290fb5b7
Festivitey <106256560+Festivitey@users.noreply.github.com>
2023-08-28 00:54:14 +0800

    Add files via upload

--- LouisHow_Project/Data/ApplicationDBContext.cs
+++ LouisHow_Project/Data/ApplicationDBContext.cs
@@ -1,14 +1,21 @@
 ﻿using LouisHow_Project.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LouisHow_Project.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
         : base (options)
         {
         }
         public DbSet<Bookings>? Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
